1303b4f77693791044e7bc357b0ca52ccff27576
王孝坤 <1101089473@qq.com>
2020-04-17 11:11:59 +0800

    两种方式实现先序遍历二叉树

--- Explore/Learn/BinaryTree/TraverseATree/preorderTraversal.cpp
+++ Explore/Learn/BinaryTree/TraverseATree/preorderTraversal.cpp
@@ -110,7 +110,7 @@ TreeNode* stringToTreeNode(string input) {
 
 void prettyPrintTree(TreeNode* node, string prefix = "", bool isLeft = true) {
     if (node == nullptr) {
-        cout << "Empty tree";
+        cout << "Empty tree"<<endl;
         return;
     }
 
@@ -153,10 +153,9 @@ public:
     void preorderTraversal_iteratively(TreeNode* root, vector<int>& vec) {
         TreeNode* tmp = root;
         stack<TreeNode*> st;
-        while (tmp)
-        {
+        while (tmp) {
+            vec.push_back(tmp->val);
             if (tmp->left) {
-                vec.push_back(tmp->val);
                 if (tmp->right) st.push(tmp);
                 tmp = tmp->left;
             }
@@ -164,15 +163,12 @@ public:
                 tmp = tmp->right;
             }
             else {
-                vec.push_back(tmp->val);
-                if (st.empty())
-                    break;
+                if (st.empty()) break;
                 tmp = st.top();
-                tmp->left = nullptr;
                 st.pop();
+                tmp = tmp->right;
             }
         }
-        return;
     }
     
     void preorderTraversal_recursively(TreeNode* root, vector<int>& vec) {
@@ -183,8 +179,13 @@ public:
     }
 };
 
-
-
+// []
+// [1]
+// [1,2]
+// [1,null,2]
+// [1,2,3,4,5,6,7]
+// [1,2,null,3,4]
+// [1,null,2,3]
 
 int main() {
     string line;
